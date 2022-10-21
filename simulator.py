d8865bef254b80c659e6399e9df6901e325f2590
Oli Smith <oliver.smith@avanade.com>
2022-10-21 14:45:16 +0100

    MVP

--- simulator.py
+++ simulator.py
@@ -11,10 +11,12 @@ leagues = {
 league_choice = {1: 'Premier League', 2: 'Championship', 3: 'League 1', 4: 'League 2'}
 
 league_averages = [80, 60, 40, 20]
-variation = 10
+team_variation = 10
+match_variation = 5
+score_weights = [10, 5, 2, 1] # 1 goal, 2, 3, 4
 
 def stat_generator(league):
-    stat = int(random.normalvariate(league_averages[league], variation))
+    stat = int(random.normalvariate(league_averages[league], team_variation))
     
     if stat > 100:
         stat = 100
@@ -33,10 +35,42 @@ def investment(money_left, ):
         print()
         moeny_input_num = int(input('Please input your amount: '))
         print()
+
+def matchday_sim(matchday_num, fix_list, league_table, league_stats):
+    results = f''
+    for fix in fix_list[matchday_num]:
+        team1, team2 = fix[0], fix[1]
         
-    
+        diff = league_stats[team1].total - league_stats[team2].total
         
+        winner = random.normalvariate(diff, match_variation)
+        
+        if winner < -5:
+            #team1 wins the match
+            team1_score = random.choices(population=[1,2,3,4], weights=score_weights, k=1)[0] #winner
+            team2_score = random.randint(0, team1_score-1)
+            
+            league_table[team1] += 3
+            league_table[team2] += 0
+            
+        elif -5 <= winner < 5:
+            team1_score = random.choices(population=[1,2,3,4], weights=score_weights, k=1)[0]
+            team2_score = team1_score
+            
+            league_table[team1] += 1
+            league_table[team2] += 1
+            
+        else:
+            #team2 wins the match
+            team2_score = random.choices(population=[1,2,3,4],weights=score_weights, k=1)[0] #winner
+            team1_score = random.randint(0, team2_score-1)
+            
+            league_table[team1] += 0
+            league_table[team2] += 3
+            
+        results += f"{team1} v {team2} : {team1_score} - {team2_score}\n"
     
+    return results
     
 class team:
     def __init__(self, league):
@@ -52,72 +86,113 @@ print('Welcome to my English Football Simulator! Soon to replace FM2022\n')
 print('The first thing you need to do is select a league to run a club in:')
 print('Input 1 for the Premier League, 2 for the Championship, 3 for League 1 and 4 for League 2.')
 
-while run_sim:
-    
+
+league_input_num = int(input('Please input your choice: '))
+print()
+
+while league_input_num not in [1,2,3,4]:
+    print('Error! Please input a number from 1 to 4 (inclusive)')
+    print()
     league_input_num = int(input('Please input your choice: '))
     print()
     
-    while league_input_num not in [1,2,3,4]:
-        print('Error! Please input a number from 1 to 4 (inclusive)')
-        print()
-        league_input_num = int(input('Please input your choice: '))
-        print()
-        
-    chosen_league_name = league_choice[league_input_num]
-    chosen_league_teams = leagues[chosen_league_name]
-        
-    print('================================================================')
-    print(f'Good choice!, you chose: {chosen_league_name}\n')
-
-    print('Now you need to choose a team in that league to manage.\n')
-    print(f'Your choices are: {leagues[chosen_league_name]}\n')
-    print('Make sure to write your choice exactly as it is written in the list above!')
+chosen_league_name = league_choice[league_input_num]
+chosen_league_teams = leagues[chosen_league_name]
     
+print('================================================================')
+print(f'Good choice!, you chose: {chosen_league_name}\n')
+
+print('Now you need to choose a team in that league to manage.\n')
+print(f'Your choices are: {leagues[chosen_league_name]}\n')
+print('Make sure to write your choice exactly as it is written in the list above!')
+
+player_team = input('Please input your choice: ')
+print()
+
+while player_team not in chosen_league_teams:
+    print('Error! Please input a team in the list of teams in your chosen league!')
+    print()
     player_team = input('Please input your choice: ')
     print()
     
-    while player_team not in chosen_league_teams:
-        print('Error! Please input a team in the list of teams in your chosen league!')
-        print()
-        player_team = input('Please input your choice: ')
-        print()
+print('================================================================')
+if player_team != 'Preston North End':
+    print(f'A great choice, you chose: {player_team} :)\n')
+else:
+    print('I cannot believe you chose them, you have poor taste in football teams for sure.\n')
+    
+print("These are your team's stats, they are ranked out of 100 each, where the higher the number the better.")
+print('These individual stats are then added together to give an overall rating for your team.\n')
+
+# Generate the rest of the teams stats in the league.
+league_stats = {team_name: team(league_input_num-1) for team_name in chosen_league_teams}
+# Select the stats for our team
+team_stats = league_stats[player_team]
+
+print(f"Manager: {team_stats.manager}\nSquad: {team_stats.squad}\nFacilities: {team_stats.facilities}\nTotal: {team_stats.total}\n")
+
+print('For reference the average total rating for your league is:', 3 * league_averages[league_input_num-1], '\n')
+
+print('================================================================')
+
+# This piece of code generates the fixture list.
+teams = random.sample(chosen_league_teams, len(chosen_league_teams))
+
+matches = []
+return_matches = []
+fixtures = []
+
+for fixture in range(1, len(chosen_league_teams)):
+
+    for i in range(int(len(chosen_league_teams)/2)):
+        matches.append((teams[i], teams[len(chosen_league_teams) - 1 - i]))
+        return_matches.append((teams[len(chosen_league_teams) - 1 - i], teams[i]))
         
-    print('================================================================')
-    if player_team != 'Preston North End':
-        print(f'A great choice, you chose: {player_team} :)\n')
-    else:
-        print('I cannot believe you chose them, you have poor taste in football teams for sure.\n')
+    teams.insert(1, teams.pop())
+    fixtures.insert(int(len(fixtures)/2), matches)
+    fixtures.append(return_matches)
+    matches = []
+    return_matches = []
+
+
+"""
+total_fixtures_list = list(permutations(chosen_league_teams, 2))
+
+for matchday in range(len(chosen_league_teams)*2 - 2):
+    fix_dict[matchday] = []
+    teams_used = []
+    
+    for first_team in chosen_league_teams:
         
-    print("These are your team's stats, they are ranked out of 100 each, where the higher the number the better.")
-    print('These individual stats are then added together to give an overall rating for your team.\n')
-    
-    # Generate the rest of the teams stats in the league.
-    league_stats = {team_name: team(league_input_num-1) for team_name in chosen_league_teams}
-    # Select the stats for our team
-    team_stats = league_stats[player_team]
-    
-    print(f"Manager: {team_stats.manager}\nSquad: {team_stats.squad}\nFacilities: {team_stats.facilities}\nTotal: {team_stats.total}\n")
-    
-    print('For reference the average total rating for your league is:', 3 * league_averages[league_input_num-1], '\n')
-    
-    print('================================================================')
-    
-    
-    total_fixtures_list = list(permutations(chosen_league_teams, 2))
-    
-    print(total_fixtures_list)
-    
-    fix_dict = {}
-    for matchday in range(len(chosen_league_teams) - 1):
-        fix_dict[matchday] = []
-        
-        for first_team in random.sample(chosen_league_teams, int(len(chosen_league_teams) / 2)):
-            fixture = random.choice([fix for fix in total_fixtures_list if first_team in fix])                                    
+        if first_team not in teams_used:
+            print([fix for fix in total_fixtures_list if first_team in fix])
+            fixture = random.choice([fix for fix in total_fixtures_list if first_team in fix])
+            while fixture[0] in teams_used or fixture[1] in teams_used:
+                fixture = random.choice([fix for fix in total_fixtures_list if first_team in fix])
+                print(first_team)
+
             fix_dict[matchday].append(fixture)
+            teams_used += [fixture[0], fixture[1]]
             total_fixtures_list.remove(fixture)
-            
-    print(fix_dict)
-        
-        
+"""
     
-    run_sim = False
+        
+# Initialise league table:
+table = {team_name: 0 for team_name in chosen_league_teams}
+
+
+print('Ready for the league to start??')
+print()
+input('Press any button to simulate the first matchday of the season!\n')
+
+for matchday in range(1, len(chosen_league_teams)*2 - 1):
+    print('================================================================')
+    print(f'Matchday {matchday}\n')
+    print('The results for this matchday are: ')
+    
+    res = matchday_sim(matchday - 1, fixtures, table, league_stats)
+    
+    print(res)
+    
+    input()
+
